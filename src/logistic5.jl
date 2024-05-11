e7e6b272ecc349ea9b8cc6d0d866cebdec48115d
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2024-05-10 23:38:29 -0300

    Better docstrings

--- src/logistic5.jl
+++ src/logistic5.jl
@@ -1,9 +1,9 @@
 """
-  `logistic5(data, params)`
+    logistic5(data, params)
 
-  Calculates the value(s) at the point(s) in `data` for the logistic5 curve with `params`.
+Calculates the value(s) at the point(s) in `data` for the logistic5 curve with `params`.
 
-  See: Cardillo G. (2012) Five parameters logistic regression - There and back again http://www.mathworks.com/matlabcentral/fileexchange/38043
+See: Cardillo G. (2012) Five parameters logistic regression - There and back again http://www.mathworks.com/matlabcentral/fileexchange/38043
 """
 @. logistic5(data, params) =
     params[4] + (params[1] - params[4]) / ((1 + (data / params[3])^params[2])^params[5])
@@ -18,19 +18,27 @@ function _p0_logistic5(rng)
 end
 
 """
-  `p0_logistic5(rng)`
+    p0_logistic5(rng)
 
-  Randomly initialize the initial logistic5 parameters to help prevent non-convergence.
+Randomly initialize the initial logistic5 parameters to help prevent non-convergence.
 """
 p0_logistic5(rng) = () -> _p0_logistic5(rng)
 
 """
-  `fit_logistic5(dissimilarities, responses; lower=Float64[0,0,0,0,0], upper=Float64[1,Inf,1,1,Inf], rng=Random.default_rng(), domain=(0,1), kwargs...)`
-  `fit_logistic5(params; domain=(0,1))`
+    fit_logistic5(dissimilarities, responses; [domain=(0,1), rng=Random.default_rng(), kwargs...])
 
-  Wrapper around `fit_model` for the `logistic5` function
+    fit_logistic5(params; [domain=(0,1)])
 
-  The `kwargs` get passed on to `curve_fit`.
+Wrapper around `fit_model` for the `logistic5` function
+
+# Arguments
+- `domain=(0,1)`: the lowest and highest values of dissimilarity. Should not typically be changed.
+- `rng=Random.default_rng()`: the `rng` to be used to generate initial values for the curve fitting.
+- `lower=[0,0,0,0,0]`: the lower bounds for each parameter.
+- `upper=[1,Inf,1,1,Inf]`: the upper bounds for each parameter.
+- `kwargs`: any other keyword argument will be passed on to the curve fitting function.
+
+The `kwargs` get passed on to `curve_fit`.
 """
 fit_logistic5(
     dissimilarities,
